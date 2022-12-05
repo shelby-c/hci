b35b7fdc2100364acbe1cb1291cac55005e3c037
sunfeihao <71101136+sunfeihao@users.noreply.github.com>
2022-12-04 19:01:11 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,218 @@
+const data=[
+  {
+    "date": "2019-12-31",
+    "state": "Alabama",
+    "gradpercent": 25.5,
+    "scores": 1143,
+    "spending": 10116.28614,
+    "revenueper": 15
+  },
+  {
+    "date": "2018-12-31",
+    "state": "California",
+    "gradpercent": 33.9,
+    "scores": 1065,
+    "spending": 14053.17631,
+    "revenueper": 21
+  },
+  {
+    "date": "2019-12-31",
+    "state": "Florida",
+    "gradpercent": 29.9,
+    "scores": 999,
+    "spending": 9936.722694,
+    "revenueper": 19
+  },
+  {
+    "date": "2019-12-31",
+    "state": "Maryland",
+    "gradpercent": 40.2,
+    "scores": 1058,
+    "spending": 	14643.81636,
+    "revenueper": 21
+  },
+  {
+    "date": "2019-12-31",
+    "state": "Massachusetts",
+    "gradpercent": 43.7,
+    "scores": 1120,
+    "spending": 18268.60485,
+    "revenueper": 3
+  },
+  {
+    "date": "2019-12-31",
+    "state": "Michigan",
+    "gradpercent": 29.1,
+    "scores": 1003,
+    "spending": 	13071.80858,
+    "revenueper": 16
+  },
+  {
+    "date": "2020-12-31",
+    "state": "Minnesota",
+    "gradpercent": 	36.1,
+    "scores": 1284,
+    "spending": 13603.18324,
+    "revenueper": 16
+  },
+  {
+    "date": "2019-12-31",
+    "state": "New Jersey",
+    "gradpercent": 39.7,
+    "scores": 1090,
+    "spending": 	20629.6443,
+    "revenueper": 25
+  },
+  {
+    "date": "2019-12-31",
+    "state": "New York",
+    "gradpercent": 36.6,
+    "scores": 1064,
+    "spending": 25520.36024,
+    "revenueper": 4
+  },
+  {
+    "date": "2019-12-31",
+    "state": "Ohio",
+    "gradpercent": 28.3,
+    "scores": 	1097,
+    "spending": 13804.54123,
+    "revenueper": 12
+  },
+  {
+    "date": "2020-12-31",
+    "state": "Oklahoma",
+    "gradpercent": 25.5,
+    "scores": 963,
+    "spending": 9512.041651,
+    "revenueper": 13
+  },
+  {
+    "date": "2019-12-31",
+    "state": "Pennsylvania",
+    "gradpercent": 	31.4,
+    "scores": 1082,
+    "spending": 17141.64616,
+    "revenueper": 3
+  },
+  {
+    "date": "2018-12-31",
+    "state": "Washington",
+    "gradpercent": 36,
+    "scores": 1074,
+    "spending": 	14555.71182,
+    "revenueper": 22
+  },
+  {
+    "date": "2019-12-31",
+    "state": "West Virginia",
+    "gradpercent": 	20.6,
+    "scores": 	943,
+    "spending": 10233.78315,
+    "revenueper": 3
+  },
+//   {
+//     "date": "2020-12-31",
+//     "state": "state E",
+//     "gradpercent": 2485,
+//     "scores": 2957.15,
+//     "spending": 472.15,
+//     "revenueper": 19
+//   },
+//   {
+//     "date": "2018-12-31",
+//     "state": "state F",
+//     "gradpercent": 1990,
+//     "scores": 2407.9,
+//     "spending": 417.9,
+//     "revenueper": 21
+//   },
+//   {
+//     "date": "2019-12-31",
+//     "state": "state F",
+//     "gradpercent": 8034,
+//     "scores": 9399.78,
+//     "spending": 1365.78,
+//     "revenueper": 17
+//   },
+//   {
+//     "date": "2020-12-31",
+//     "state": "state F",
+//     "gradpercent": 13254,
+//     "scores": 14314.32,
+//     "spending": 1060.32,
+//     "revenueper": 8
+//   },
+//   {
+//     "date": "2018-12-31",
+//     "state": "state G",
+//     "gradpercent": 18545,
+//     "scores": 21141.3,
+//     "spending": 2596.3,
+//     "revenueper": 14
+//   },
+//   {
+//     "date": "2019-12-31",
+//     "state": "state G",
+//     "gradpercent": 6168,
+//     "scores": 7524.96,
+//     "spending": 1356.96,
+//     "revenueper": 22
+//   },
+//   {
+//     "date": "2020-12-31",
+//     "state": "state G",
+//     "gradpercent": 12382,
+//     "scores": 14610.76,
+//     "spending": 2228.76,
+//     "revenueper": 18
+//   },
+//   {
+//     "date": "2018-12-31",
+//     "state": "state H",
+//     "gradpercent": 12498,
+//     "scores": 14622.66,
+//     "spending": 2124.66,
+//     "revenueper": 17
+//   },
+//   {
+//     "date": "2019-12-31",
+//     "state": "state H",
+//     "gradpercent": 8214,
+//     "scores": 8953.26,
+//     "spending": 739.26,
+//     "revenueper": 9
+//   },
+//   {
+//     "date": "2020-12-31",
+//     "state": "state H",
+//     "gradpercent": 3361,
+//     "scores": 3865.15,
+//     "spending": 504.15,
+//     "revenueper": 15
+//   },
+//   {
+//     "date": "2018-12-31",
+//     "state": "state I",
+//     "gradpercent": 16152,
+//     "scores": 18413.28,
+//     "spending": 2261.28,
+//     "revenueper": 14
+//   },
+//   {
+//     "date": "2019-12-31",
+//     "state": "state I",
+//     "gradpercent": 2303,
+//     "scores": 2349.06,
+//     "spending": 46.06,
+//     "revenueper": 2
+//   },
+//   {
+//     "date": "2020-12-31",
+//     "state": "state I",
+//     "gradpercent": 14806,
+//     "scores": 18063.32,
+//     "spending": 3257.32,
+//     "revenueper": 22
+//   }
+];
